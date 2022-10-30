8d472088a1baad25c7f8a639b2e7ca1110fa397e
Diego Chaparro <torresfeliped@gmail.com>
2022-10-30 00:49:22 -0500

    Se carga proyecto Api_NetCore

---
+++ API_NetCore/API_NetCore/Controllers/BooksController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_NetCore.Data;
+using API_NetCore.Models;
+using System.Text.Json;
+
+namespace API_NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly API_NetCoreContext _context;
+
+        public BooksController(API_NetCoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBook()
+        {
+            return await _context.Book.ToListAsync();
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Book.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        // PUT: api/Books/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest("Peticion no valida");
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            JsonResult Messagge = new JsonResult("Peticion Valida");
+            return Ok(Messagge);
+        }
+
+        // POST: api/Books
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult> PostBook(Book book)
+        {
+            _context.Book.Add(book);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetBook", new { id = book.Id }, book);
+            //JsonResult Messagge = new JsonResult("Peticion Valida");
+            //JsonSerializerOptions variable = new JsonSerializerOptions();
+            return Ok(new { messagge = "Peticion Valida" });
+        }
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Book>> DeleteBook(int id)
+        {
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Book.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return book;
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Book.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ API_NetCore/API_NetCore/Data/API_NetCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API_NetCore.Models;
+
+namespace API_NetCore.Data
+{
+    public class API_NetCoreContext : DbContext
+    {
+        public API_NetCoreContext (DbContextOptions<API_NetCoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<API_NetCore.Models.Book> Book { get; set; }
+    }
+}
