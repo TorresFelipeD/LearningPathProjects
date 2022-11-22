342d3a4bc74a9f8d08fdac798bfa9af8aef8e256
Diego Chaparro <torresfeliped@gmail.com>
2022-11-21 23:17:53 -0500

    Se agrega proyecto fundamentos .net platzi

---
+++ FundamentosDotNet/InfoConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using Humanizer;
+using System.Globalization;
+
+Console.WriteLine("Por favor ingrese un nombre:");
+string name = Console.ReadLine(); 
+Console.WriteLine("Por favor ingrese un cargo:");
+string position = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su edad:");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hola, su nombre es {name}, su cargo es {position} y su edad es {age.ToWords(new CultureInfo("es"))}");
