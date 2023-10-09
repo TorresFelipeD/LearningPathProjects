9b48d7dbb92f3d9c0b674005ac6c45b99bcbb248
Diego Felipe Chaparro Torres <dchaparro@axede.com.co>
2023-10-09 09:19:26 -0500

    Se cargan archivos otros

---
+++ Others/Api-Gateway/API-Gateway-Ocelot-NET/README.md
@@ -0,0 +1,7 @@
+# API Gateway Ocelot .NET
+Example of API Gateway in .NET with Ocelot. This example contains 3 microservices and an API gateway. Configuration and some of the Ocelot features such as authentication, authorization, caching, rate limiting, delegating handlers, etc.
+
+## Tecnologías
+
+* [ASP.NET Core 7](https://docs.microsoft.com/en-us/aspnet/core/introduction-to-aspnet-core)
+* [Ocelot](https://github.com/ThreeMammals/Ocelot)
---
+++ Others/Test-Code/Test-Code/Test-Code/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Numerics;
+
+namespace Test_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string binario = "1111010101111"; // Cambia esto por tu número binario
+            int countOperations = solution(binario);
+            Console.WriteLine("El número decimal es: " + countOperations);
+
+            string binaryString = new string('1', 400000); // Representación de "1" repetido 400,000 veces en binario
+            BigInteger res = 0;
+
+            // I'm totally skipping error handling here
+            foreach (char c in binaryString)
+            {
+                res <<= 1;
+                res += c == '1' ? 1 : 0;
+            }
+
+            Console.WriteLine("Valor representado como BigInteger:");
+            Console.WriteLine(res.ToString());
+        }
+
+        public static int solution(string S)
+        {
+            int countOperations = 0;
+            var decimalNumber = Convert.ToInt64(S, 2);
+
+            while (decimalNumber>0)
+            {
+                if(decimalNumber % 2 == 0)
+                {
+                    decimalNumber/=2;
+                    countOperations++;
+                }
+                else
+                {
+                    decimalNumber--;
+                    countOperations++;
+                }
+            }
+
+            return countOperations;
+        }
+    }
+}
